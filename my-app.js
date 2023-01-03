b69ff9d8d53856ae68e23549c89a7a7977bf5517
Afaq-302 <ufaq302@yahoo.com>
2023-01-03 11:15:50 +0500

    Update my app.js

--- my-app.js
+++ my-app.js
@@ -1,30 +1,28 @@
 //jshint esversion:6
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const _ = require("lodash")
+const mongoose = require("mongoose");
+const _=require("lodash")
+
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1:27017/newDB");
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
-//Schema for Items
-const itemsSchema = { name: String }
+const itemsSchema = {
+  name: String
+}
 
-//Creating Mongoose model using Item Schema
 const Item = mongoose.model("Item", itemsSchema);
 
-//Creating Sample Document
 const item1 = new Item({
   name: "This is a Sample Todo!"
 });
+
 
 const defaultItems = [item1];
 
@@ -35,16 +33,16 @@ const listSchema = {
 
 const List = mongoose.model("List", listSchema)
 
-
 app.get("/", function (req, res) {
 
   Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
+
       Item.insertMany(defaultItems, function (err) {
         if (err) {
           console.log(err);
         } else {
-          console.log("Saved to Database!!");
+          console.log("Saved To DB Successfully!");
         }
       });
 
@@ -55,12 +53,11 @@ app.get("/", function (req, res) {
   });
 });
 
+
 app.post("/", function (req, res) {
 
   const itemName = req.body.newItem;
   const listName = req.body.list;
-
-  console.log(req.body);
 
   const item = new Item({
     name: itemName
@@ -69,6 +66,7 @@ app.post("/", function (req, res) {
   if (listName === "Today") {
     item.save();
     res.redirect("/");
+
   } else {
     List.findOne({ name: listName }, function (err, foundList) {
       foundList.items.push(item);
@@ -79,29 +77,37 @@ app.post("/", function (req, res) {
 });
 
 app.post("/delete", function (req, res) {
-
-  res.redirect("/");
+  
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
-  Item.findByIdAndRemove(checkedItemId, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Deleted Successfully!");
-    }
-  })
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully deleted Item!");
+      }
+      res.redirect("/");
+    });
+  } else {
+    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
+      if (!err) {
+        res.redirect("/" + listName)
+      }
+    });
+  }
+
+
 });
 
-//for now the customList wil just log the customList Name
-app.get("/:customList", function (req, res) {
-
-  const customListName = _.capitalize(req.params.customList);
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, function (err, foundList) {
     if (!err) {
-
       if (!foundList) {
+
         //Create a new list
         const list = new List({
           name: customListName,
@@ -109,30 +115,25 @@ app.get("/:customList", function (req, res) {
         });
 
         list.save();
-        res.redirect("/" + customListName)
+        res.redirect("/" + customListName);
 
       } else {
-        res.render("list", { listTitle: foundList.name, newListItems: foundList.items })
+
+        //Show an existing list
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
       }
     }
-
   });
-
-
-
-  console.log(customListName);
-
-  // res.redirect("/");
-
 });
+
+
+
+
 
 
 app.get("/about", function (req, res) {
   res.render("about");
 });
-
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
